cab391c4b82c76747b4d0498eff115663334479e
Apeksha43 <sapeksha43@gmail.com>
2020-08-18 03:11:41 -0400

    added configs

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 'use strict'
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 3007
+const httpHandler = require('./src/HttpHandler')
 
+app.use(cors())
+app.use(express.static('./public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
+app.post('/addTrack', httpHandler.addTrack)
 
 app.listen(port, function () {
     console.log('server listening on : http://localhost:%s', port)
